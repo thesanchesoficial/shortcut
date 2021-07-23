451f521e9bbf03125c82a0e36ca6079c69783867
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2021-07-22 23:34:54 -0300

    file image

--- android/src/main/java/com/nightmare/shortcut/ShortcutPlugin.java
+++ android/src/main/java/com/nightmare/shortcut/ShortcutPlugin.java
@@ -27,14 +27,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/**
- * ShortcutPlugin
- */
 public class ShortcutPlugin implements FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     Context mContext;
     private FlutterPluginBinding flutterPluginBinding;
@@ -61,26 +54,39 @@ public class ShortcutPlugin implements FlutterPlugin, MethodCallHandler {
             }
             shortcutInfoIntent.setAction(Intent.ACTION_MAIN);
             String key = flutterPluginBinding.getFlutterAssets().getAssetFilePathByName(map.get("asset"));
-            addShortcut((String) call.argument("name"), shortcutInfoIntent,getImageFromAssetsFile(key));
+            addShortcut((String) call.argument("name"), shortcutInfoIntent, getImageFromAssetsFile(key));
             result.success("Android " + android.os.Build.VERSION.RELEASE);
         } else {
             result.notImplemented();
         }
     }
-    private Bitmap getImageFromAssetsFile(String fileName)
-    {
+    private Bitmap getImageFromAssetsFile(String fileName) {
+
+        System.out.println("AAAAAA");
+        System.out.println(fileName);
+        System.out.println("BBBBBB");
+
         Bitmap image = null;
 
-        AssetManager am =  flutterPluginBinding.getApplicationContext().getAssets();
+        Bitmap bitmap = BitmapFactory.decodeFile(fileName);
 
-        try {
-            InputStream is = am.open(fileName);
-            image = BitmapFactory.decodeStream(is);
-            is.close();
+        if(fileName.contains("file:///")) {
+            image = bitmap;
+        } else {
+
+            AssetManager am =  flutterPluginBinding.getApplicationContext().getAssets();
+
+            try {
+                InputStream is = am.open(fileName);
+                image = BitmapFactory.decodeStream(is);
+                is.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        
         return image;
     }
     @Override
@@ -88,47 +94,38 @@ public class ShortcutPlugin implements FlutterPlugin, MethodCallHandler {
         channel.setMethodCallHandler(null);
     }
 
-    /**
-     * 添加桌面图标快捷方式
-     *
-     * @param name         快捷方式名称
-     * @param actionIntent 快捷方式图标点击动作
-     */
-    public void addShortcut(String name, Intent actionIntent,Bitmap bitmap) {
+    public void addShortcut(String name, Intent actionIntent, Bitmap bitmap) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            //  创建快捷方式的intent广播
             Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-            // 添加快捷名称
             shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-            //  快捷图标是允许重复(不一定有效)
             shortcut.putExtra("duplicate", false);
-            // 快捷图标
-            // 使用资源id方式
-//            Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(activity, R.mipmap.icon);
-//            shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
-            // 使用Bitmap对象模式
             shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
-            // 添加携带的下次启动要用的Intent信息
             shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, actionIntent);
-            // 发送广播
             mContext.sendBroadcast(shortcut);
         } else {
             ShortcutManager shortcutManager = (ShortcutManager) mContext.getSystemService(Context.SHORTCUT_SERVICE);
             if (null == shortcutManager) {
-                // 创建快捷方式失败
                 Log.e("MainActivity", "Create shortcut failed");
                 return;
             }
             ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(mContext, name)
-                    .setShortLabel(name)
-                    .setIntent(actionIntent)
-                    .setLongLabel(name)
-                    .setIcon(Icon.createWithBitmap(bitmap))
-                    .build();
+                .setShortLabel(name)
+                .setIntent(actionIntent)
+                .setLongLabel(name)
+                .setIcon(Icon.createWithBitmap(bitmap))
+                .build();
 
-            PendingIntent shortcutCallbackIntent = PendingIntent.getBroadcast(mContext, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent shortcutCallbackIntent = PendingIntent.getBroadcast(
+                mContext, 
+                0, 
+                actionIntent, 
+                PendingIntent.FLAG_UPDATE_CURRENT
+            );
 
-            shortcutManager.requestPinShortcut(shortcutInfo, shortcutCallbackIntent.getIntentSender());
+            shortcutManager.requestPinShortcut(
+                shortcutInfo, 
+                shortcutCallbackIntent.getIntentSender()
+            );
         }
     }
 }
